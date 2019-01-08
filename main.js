3d2a0eba8bfaeace889c0761790755f574116246
ray-nis <reirei256@gmail.com>
2019-01-08 06:24:59 +0100

    experience and menu change on scroll

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 $("#menuIcon").click(function(){
     $("#nav-bar").slideToggle();
   });
+
+$( document ).ready(function() {
+    $(window).scroll(function() {
+        var Scroll = $(window).scrollTop() + 1;
+        var aboutSection = $('#about').offset().top + 1;
+        var experienceSection = $('#experience').offset().top;
+
+        if (Scroll >= aboutSection && Scroll < experienceSection) {
+            $("#m1").addClass("current-menu-item");
+            $("#m2").removeClass("current-menu-item");
+        }
+        else if (Scroll >= experienceSection) {
+            $("#m2").addClass("current-menu-item");
+            $("#m1").removeClass("current-menu-item");
+        }
+        console.log("scr = " + Scroll);
+        console.log(" abo = " + aboutSection);
+        console.log("exp = " + experienceSection);
+    });
+});
