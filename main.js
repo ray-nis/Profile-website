337d166832d7a5471f32914152edc2c2bab94300
ray-nis <reirei256@gmail.com>
2019-01-09 01:34:42 +0100

    scroll

--- main.js
+++ main.js
@@ -7,18 +7,36 @@ $( document ).ready(function() {
         var Scroll = $(window).scrollTop() + 1;
         var aboutSection = $('#about').offset().top + 1;
         var experienceSection = $('#experience').offset().top;
+        var educationSection = $('#education').offset().top;
+        var skillsSection = $('#skills').offset().top;
 
         if (Scroll >= aboutSection && Scroll < experienceSection) {
             $("#m1").addClass("current-menu-item");
             $("#m2").removeClass("current-menu-item");
+            $("#m3").removeClass("current-menu-item");
+            $("#m4").removeClass("current-menu-item");
         }
-        else if (Scroll >= experienceSection) {
+        else if (Scroll >= experienceSection && Scroll < educationSection) {
             $("#m2").addClass("current-menu-item");
             $("#m1").removeClass("current-menu-item");
+            $("#m3").removeClass("current-menu-item");
+            $("#m4").removeClass("current-menu-item");
         }
-        console.log("scr = " + Scroll);
-        console.log(" abo = " + aboutSection);
-        console.log("exp = " + experienceSection);
+        else if (Scroll >= educationSection && Scroll < skillsSection) {
+            $("#m3").addClass("current-menu-item");
+            $("#m1").removeClass("current-menu-item");
+            $("#m2").removeClass("current-menu-item");
+            $("#m4").removeClass("current-menu-item");
+        }
+        else if(Scroll >= skillsSection) {
+            $("#m4").addClass("current-menu-item");
+            $("#m1").removeClass("current-menu-item");
+            $("#m2").removeClass("current-menu-item");
+            $("#m3").removeClass("current-menu-item");
+        }
+
+        console.log("scro = " + Scroll);
+        console.log("sk = " + skillsSection)
     });
 });
 
