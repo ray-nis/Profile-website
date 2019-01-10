d0e0f6656e204ec05d5a758aaf7bb1cd8bb1524f
ray-nis <reirei256@gmail.com>
2019-01-10 03:34:36 +0100

    menu

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ $( document ).ready(function() {
         var experienceSection = $('#experience').offset().top;
         var educationSection = $('#education').offset().top;
         var skillsSection = $('#skills').offset().top;
+        var interestsSection = $('#interests').offset().top;
+        var awardsSection = $('#awards').offset().top;
 
         if (Scroll >= aboutSection && Scroll < experienceSection) {
             $("#m1").addClass("current-menu-item");
@@ -21,22 +23,46 @@ $( document ).ready(function() {
             $("#m1").removeClass("current-menu-item");
             $("#m3").removeClass("current-menu-item");
             $("#m4").removeClass("current-menu-item");
+            $("#m5").removeClass("current-menu-item");
+            $("#m6").removeClass("current-menu-item");
         }
         else if (Scroll >= educationSection && Scroll < skillsSection) {
             $("#m3").addClass("current-menu-item");
             $("#m1").removeClass("current-menu-item");
             $("#m2").removeClass("current-menu-item");
             $("#m4").removeClass("current-menu-item");
+            $("#m5").removeClass("current-menu-item");
+            $("#m6").removeClass("current-menu-item");
         }
-        else if(Scroll >= skillsSection) {
+        else if(Scroll >= skillsSection && Scroll < interestsSection) {
             $("#m4").addClass("current-menu-item");
             $("#m1").removeClass("current-menu-item");
             $("#m2").removeClass("current-menu-item");
             $("#m3").removeClass("current-menu-item");
+            $("#m5").removeClass("current-menu-item");
+            $("#m6").removeClass("current-menu-item");
+        }
+        else if(Scroll >= interestsSection && Scroll < awardsSection) {
+            $("#m5").addClass("current-menu-item");
+            $("#m1").removeClass("current-menu-item");
+            $("#m2").removeClass("current-menu-item");
+            $("#m3").removeClass("current-menu-item");
+            $("#m4").removeClass("current-menu-item");
+            $("#m6").removeClass("current-menu-item");
+        }
+        else if(Scroll >= (awardsSection - 250)) {
+            $("#m6").addClass("current-menu-item");
+            $("#m1").removeClass("current-menu-item");
+            $("#m2").removeClass("current-menu-item");
+            $("#m3").removeClass("current-menu-item");
+            $("#m4").removeClass("current-menu-item");
+            $("#m5").removeClass("current-menu-item");
         }
 
-        console.log("scro = " + Scroll);
-        console.log("sk = " + skillsSection)
+        console.log("scr =" + Scroll);
+        console.log("awards =" + awardsSection)
+
+        
     });
 });
 
